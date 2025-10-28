93d2581c065cf98ffd34a11b5f4847f7b11f08a0
Hugo Svirak <hugo@hugosvirak.com>
2025-10-28 10:40:14 +0100

    Remove pixi.js external

--- src/external/pixi.js/filters/defaults/defaultFilter.vert.mjs
+++
@@ -1,4 +0,0 @@
-var vertex = "in vec2 aPosition;\r\nout vec2 vTextureCoord;\r\n\r\nuniform vec4 uInputSize;\r\nuniform vec4 uOutputFrame;\r\nuniform vec4 uOutputTexture;\r\n\r\nvec4 filterVertexPosition( void )\r\n{\r\n    vec2 position = aPosition * uOutputFrame.zw + uOutputFrame.xy;\r\n    \r\n    position.x = position.x * (2.0 / uOutputTexture.x) - 1.0;\r\n    position.y = position.y * (2.0*uOutputTexture.z / uOutputTexture.y) - uOutputTexture.z;\r\n\r\n    return vec4(position, 0.0, 1.0);\r\n}\r\n\r\nvec2 filterTextureCoord( void )\r\n{\r\n    return aPosition * (uOutputFrame.zw * uInputSize.zw);\r\n}\r\n\r\nvoid main(void)\r\n{\r\n    gl_Position = filterVertexPosition();\r\n    vTextureCoord = filterTextureCoord();\r\n}\r\n";
-
-export { vertex as default };
-//# sourceMappingURL=defaultFilter.vert.mjs.map
--- src/external/pixi.js/filters/defaults/passthrough/passthrough.frag.mjs
+++
@@ -1,4 +0,0 @@
-var fragment = "in vec2 vTextureCoord;\r\nout vec4 finalColor;\r\nuniform sampler2D uTexture;\r\nvoid main() {\r\n    finalColor = texture(uTexture, vTextureCoord);\r\n}\r\n";
-
-export { fragment as default };
-//# sourceMappingURL=passthrough.frag.mjs.map
